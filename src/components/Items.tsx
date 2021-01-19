90618d2a5d56742a6e9eba199b557bcfe654093a
ozaki25 <fs65ugumori@yahoo.co.jp>
2021-01-19 23:32:11 +0900

    fixed tabIndex value

--- src/components/Items.tsx
+++ src/components/Items.tsx
@@ -2,7 +2,7 @@ import React, { forwardRef } from 'react';
 
 function Items({ items }, ref) {
   return (
-    <ul ref={ref} tabIndex="-1" style={{ outline: 'none' }}>
+    <ul ref={ref} tabIndex={-1} style={{ outline: 'none' }}>
       {items.map(item => (
         <li key={item.id}>
           <a href={item.url}>{item.title}</a>
